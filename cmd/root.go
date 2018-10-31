8e73e16d67fad056fc28897af74e42830dbf1624
Myles Horton <myles@getlantern.org>
2018-10-31 16:20:57 -0700

    progress

--- cmd/root.go
+++ cmd/root.go
@@ -226,12 +226,14 @@ func (m *migrator) migrateWith(sc scan, kl klen) {
 
 func (m *migrator) newBar(multi *mpb.Progress, length int64, name string) *mpb.Bar {
 	return multi.AddBar(length,
-		mpb.PrependDecorators(
-			decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
-			//decor.Name(name),
-			// decor.DSyncWidth bit enables column width synchronization
-			//decor.Percentage(decor.WCSyncSpace),
-		),
+		/*
+			mpb.PrependDecorators(
+				decor.Name(name, decor.WC{W: len(name) + 1, C: decor.DidentRight}),
+				//decor.Name(name),
+				// decor.DSyncWidth bit enables column width synchronization
+				//decor.Percentage(decor.WCSyncSpace),
+			),
+		*/
 		mpb.AppendDecorators(
 			// replace ETA decorator with "done" message, OnComplete event
 			decor.OnComplete(
@@ -239,6 +241,7 @@ func (m *migrator) newBar(multi *mpb.Progress, length int64, name string) *mpb.B
 				decor.EwmaETA(decor.ET_STYLE_GO, 60), "done",
 			),
 			decor.Percentage(decor.WCSyncSpace),
+			decor.Name(name),
 		),
 	)
 }
